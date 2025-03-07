a42b0f2ee06d9834efa778b75c74372b19fc10ed
Jark Wu <jark@apache.org>
2025-03-07 18:10:58 +0800

    hotfix

--- fluss-lakehouse/fluss-lakehouse-paimon/src/main/java/com/alibaba/fluss/lakehouse/paimon/source/enumerator/FlinkSourceEnumerator.java
+++ fluss-lakehouse/fluss-lakehouse-paimon/src/main/java/com/alibaba/fluss/lakehouse/paimon/source/enumerator/FlinkSourceEnumerator.java
@@ -599,7 +599,6 @@ public class FlinkSourceEnumerator
                         removedPartitions.add(p);
                     }
                 });
-        ;
 
         if (!removedPartitions.isEmpty()) {
             LOG.info("Discovered removed partitions: {}", removedPartitions);
