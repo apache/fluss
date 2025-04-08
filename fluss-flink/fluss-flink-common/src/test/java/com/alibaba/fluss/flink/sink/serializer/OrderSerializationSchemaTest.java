d98edad276113d982eaa30fc1f777f261cd18850
Mehul Batra <mehulbatra1997@gmail.com>
2025-04-08 15:23:41 +0530

    Fix License

--- fluss-flink/fluss-flink-common/src/test/java/com/alibaba/fluss/flink/sink/serializer/OrderSerializationSchemaTest.java
+++ fluss-flink/fluss-flink-common/src/test/java/com/alibaba/fluss/flink/sink/serializer/OrderSerializationSchemaTest.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) 2025 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.alibaba.fluss.flink.sink.serializer;
 
 import com.alibaba.fluss.flink.common.Order;
@@ -6,6 +22,7 @@ import com.alibaba.fluss.types.DataField;
 import com.alibaba.fluss.types.IntType;
 import com.alibaba.fluss.types.RowType;
 import com.alibaba.fluss.types.StringType;
+
 import org.apache.flink.table.data.RowData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
