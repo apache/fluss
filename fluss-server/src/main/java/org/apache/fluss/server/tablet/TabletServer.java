962777e87b5fd602bb469c8b4918393462ea77dd
yunhong <337361684@qq.com>
2025-07-23 11:46:19 +0800

    address jark's comments

--- fluss-server/src/main/java/org/apache/fluss/server/tablet/TabletServer.java
+++ fluss-server/src/main/java/org/apache/fluss/server/tablet/TabletServer.java
@@ -35,7 +35,6 @@ import org.apache.fluss.rpc.messages.ControlledShutdownResponse;
 import org.apache.fluss.rpc.metrics.ClientMetricGroup;
 import org.apache.fluss.rpc.netty.server.RequestsMetrics;
 import org.apache.fluss.server.ServerBase;
-import org.apache.fluss.server.ServerState;
 import org.apache.fluss.server.authorizer.Authorizer;
 import org.apache.fluss.server.authorizer.AuthorizerLoader;
 import org.apache.fluss.server.coordinator.MetadataManager;
@@ -203,7 +202,6 @@ public class TabletServer extends ServerBase {
 
             this.logManager = LogManager.create(conf, zkClient, scheduler, clock);
             logManager.startup();
-            serverState = ServerState.RECOVERY;
 
             this.kvManager = KvManager.create(conf, zkClient, logManager);
             kvManager.startup();
@@ -276,7 +274,6 @@ public class TabletServer extends ServerBase {
 
             controlledShutDown();
 
-            serverState = ServerState.SHUTTING_DOWN;
             CompletableFuture<Void> serviceShutdownFuture = stopServices();
 
             serviceShutdownFuture.whenComplete(
@@ -288,8 +285,6 @@ public class TabletServer extends ServerBase {
                         }
                     }));
         }
-
-        serverState = ServerState.NOT_RUNNING;
 
         return terminationFuture;
     }
@@ -428,7 +423,6 @@ public class TabletServer extends ServerBase {
 
     private void controlledShutDown() {
         LOG.info("Starting controlled shutdown.");
-        serverState = ServerState.PENDING_CONTROLLED_SHUTDOWN;
 
         // We request the CoordinatorServer to do a controlled shutdown. On failure, we backoff for
         // a period of time and try again for a number of retries. If all the attempt fails, we
