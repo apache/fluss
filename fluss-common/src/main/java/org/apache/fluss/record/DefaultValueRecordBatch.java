084894174b039a915a42c99c410668349cb9aabb
Jark Wu <jark@apache.org>
2025-08-25 13:55:20 +0800

    [infra] Update package and groupId and spotless for org.apache.fluss

--- fluss-common/src/main/java/org/apache/fluss/record/DefaultValueRecordBatch.java
+++ fluss-common/src/main/java/org/apache/fluss/record/DefaultValueRecordBatch.java
@@ -273,7 +273,9 @@ public class DefaultValueRecordBatch implements ValueRecordBatch {
             currentRecordNumber++;
         }
 
-        /** @param valueBytes consisted of schema id and the row encoded in the value bytes */
+        /**
+         * @param valueBytes consisted of schema id and the row encoded in the value bytes
+         */
         public void append(byte[] valueBytes) throws IOException {
             if (isClosed) {
                 throw new IllegalStateException(
