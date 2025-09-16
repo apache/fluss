5cc22efa83bb7bf1843971d371581eb1978745f5
caozhen1937 <caozhen1937@163.com>
2025-09-16 11:59:31 +0800

    [server] Forbid any table/partition creation that begins with `__`

--- fluss-common/src/main/java/org/apache/fluss/metadata/TablePath.java
+++ fluss-common/src/main/java/org/apache/fluss/metadata/TablePath.java
@@ -54,6 +54,8 @@ public class TablePath implements Serializable {
     // RecordAccumulator.ready)
     private Integer hash;
 
+    private static final String INTERNAL_NAME_PREFIX = "__";
+
     public TablePath(String databaseName, String tableName) {
         this.databaseName = databaseName;
         this.tableName = tableName;
@@ -127,18 +129,33 @@ public class TablePath implements Serializable {
 
     public static void validateDatabaseName(String databaseName) throws InvalidDatabaseException {
         String dbError = detectInvalidName(databaseName);
-        if (dbError != null) {
+        String dbInternalNameError = validatePrefix(databaseName);
+        if (dbError != null || dbInternalNameError != null) {
             throw new InvalidDatabaseException(
-                    "Database name " + databaseName + " is invalid: " + dbError);
+                    "Database name "
+                            + databaseName
+                            + " is invalid: "
+                            + (dbError != null ? dbError : dbInternalNameError));
         }
     }
 
     public static void validateTableName(String tableName) throws InvalidTableException {
         String tableError = detectInvalidName(tableName);
-        if (tableError != null) {
+        String tableInternalNameError = validatePrefix(tableName);
+        if (tableError != null || tableInternalNameError != null) {
             throw new InvalidTableException(
-                    "Table name " + tableName + " is invalid: " + tableError);
+                    "Table name "
+                            + tableName
+                            + " is invalid: "
+                            + (tableError != null ? tableError : tableInternalNameError));
         }
+    }
+
+    public static String validatePrefix(String identifier) throws InvalidTableException {
+        if (identifier != null && identifier.startsWith(INTERNAL_NAME_PREFIX)) {
+            return "'" + INTERNAL_NAME_PREFIX + "' is not allowed as prefix";
+        }
+        return null;
     }
 
     public static String detectInvalidName(String identifier) {
