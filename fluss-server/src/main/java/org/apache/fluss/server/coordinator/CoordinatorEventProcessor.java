05fb8de7a302ebace1ebb8f7c734dc06cf15d53a
Liebing <liebingyu@gmail.com>
2025-10-23 17:36:01 +0800

    [server] Servers with PERMANENT_OFFLINE and TEMPORARY_OFFLINE tag are no longer assigned replicas

--- fluss-server/src/main/java/org/apache/fluss/server/coordinator/CoordinatorEventProcessor.java
+++ fluss-server/src/main/java/org/apache/fluss/server/coordinator/CoordinatorEventProcessor.java
@@ -239,7 +239,9 @@ public class CoordinatorEventProcessor implements EventProcessor {
         HashSet<ServerInfo> tabletServerInfoList =
                 new HashSet<>(coordinatorContext.getLiveTabletServers().values());
         serverMetadataCache.updateMetadata(
-                coordinatorContext.getCoordinatorServerInfo(), tabletServerInfoList);
+                coordinatorContext.getCoordinatorServerInfo(),
+                tabletServerInfoList,
+                coordinatorContext.getServerTags());
         updateTabletServerMetadataCacheWhenStartup(tabletServerInfoList);
 
         // start table manager
@@ -856,7 +858,8 @@ public class CoordinatorEventProcessor implements EventProcessor {
         // update coordinatorServer metadata cache for the new added table server.
         serverMetadataCache.updateMetadata(
                 coordinatorContext.getCoordinatorServerInfo(),
-                new HashSet<>(coordinatorContext.getLiveTabletServers().values()));
+                new HashSet<>(coordinatorContext.getLiveTabletServers().values()),
+                coordinatorContext.getServerTags());
         // update server info for all tablet servers.
         updateTabletServerMetadataCache(
                 new HashSet<>(coordinatorContext.getLiveTabletServers().values()),
@@ -913,7 +916,9 @@ public class CoordinatorEventProcessor implements EventProcessor {
                 new HashSet<>(coordinatorContext.getLiveTabletServers().values());
         // update coordinatorServer metadata cache.
         serverMetadataCache.updateMetadata(
-                coordinatorContext.getCoordinatorServerInfo(), serverInfos);
+                coordinatorContext.getCoordinatorServerInfo(),
+                serverInfos,
+                coordinatorContext.getServerTags());
         updateTabletServerMetadataCache(serverInfos, null, null, Collections.emptySet());
 
         TableBucketStateMachine tableBucketStateMachine = tableManager.getTableBucketStateMachine();
@@ -986,6 +991,11 @@ public class CoordinatorEventProcessor implements EventProcessor {
 
         // Then update coordinatorContext.
         serverIds.forEach(serverId -> coordinatorContext.putServerTag(serverId, serverTag));
+        // update coordinatorServer metadata cache for the new added serverTag
+        serverMetadataCache.updateMetadata(
+                coordinatorContext.getCoordinatorServerInfo(),
+                new HashSet<>(coordinatorContext.getLiveTabletServers().values()),
+                coordinatorContext.getServerTags());
 
         return addServerTagResponse;
     }
@@ -1028,6 +1038,11 @@ public class CoordinatorEventProcessor implements EventProcessor {
 
         // Then update coordinatorContext.
         serverIds.forEach(coordinatorContext::removeServerTag);
+        // update coordinatorServer metadata cache for the new removed serverTag
+        serverMetadataCache.updateMetadata(
+                coordinatorContext.getCoordinatorServerInfo(),
+                new HashSet<>(coordinatorContext.getLiveTabletServers().values()),
+                coordinatorContext.getServerTags());
 
         return removeServerTagResponse;
     }
