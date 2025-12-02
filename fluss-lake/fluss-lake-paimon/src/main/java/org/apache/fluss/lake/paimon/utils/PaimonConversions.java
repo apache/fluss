3e046d4953875c14ac6a13d66cb07660577996b9
Liebing <liebingyu@gmail.com>
2025-12-02 20:07:08 +0800

    [server] Support Rescale Bucket for Log Table Without Bucket Keys

--- fluss-lake/fluss-lake-paimon/src/main/java/org/apache/fluss/lake/paimon/utils/PaimonConversions.java
+++ fluss-lake/fluss-lake-paimon/src/main/java/org/apache/fluss/lake/paimon/utils/PaimonConversions.java
@@ -55,6 +55,9 @@ public class PaimonConversions {
     // again
     private static final String PARTITION_GENERATE_LEGACY_NAME_OPTION_KEY = "partition.legacy-name";
 
+    // property key for bucket number
+    private static final String BUCKET_NUM = "bucket.num";
+
     // for fluss config
     public static final String FLUSS_CONF_PREFIX = "fluss.";
     // for paimon config
@@ -117,7 +120,14 @@ public class PaimonConversions {
         List<SchemaChange> schemaChanges = new ArrayList<>(tableChanges.size());
 
         for (TableChange tableChange : tableChanges) {
-            if (tableChange instanceof TableChange.SetOption) {
+            if (tableChange instanceof TableChange.BucketNumOption) {
+                TableChange.BucketNumOption bucketNumOption =
+                        (TableChange.BucketNumOption) tableChange;
+                schemaChanges.add(
+                        SchemaChange.setOption(
+                                convertFlussPropertyKeyToPaimon(BUCKET_NUM),
+                                String.valueOf(bucketNumOption.getBucketNum())));
+            } else if (tableChange instanceof TableChange.SetOption) {
                 TableChange.SetOption setOption = (TableChange.SetOption) tableChange;
                 schemaChanges.add(
                         SchemaChange.setOption(
