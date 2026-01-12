a1c09ab8712a2c8f9021914279592b5865460864
羊川 <yangchuan.zy@alibaba-inc.com>
2026-01-12 10:04:37 +0800

    fix comments

--- fluss-spark/fluss-spark-ut/src/test/scala/org/apache/fluss/spark/SparkCatalogTest.scala
+++ fluss-spark/fluss-spark-ut/src/test/scala/org/apache/fluss/spark/SparkCatalogTest.scala
@@ -192,60 +192,53 @@ class SparkCatalogTest extends FlussSparkTestBase {
   }
 
   test("show partitions") {
-    withCurrentCatalogAndNamespace {
-      withTable("t") {
-        sql(s"CREATE TABLE t (id int, name string, pt1 string, pt2 int) PARTITIONED BY (pt1, pt2)")
-        sql(s"INSERT INTO t values(1, 'a', 'a', 1), (2, 'b', 'a', 2), (3, 'c', 'c', 3)")
+    withTable("t") {
+      sql(s"CREATE TABLE t (id int, name string, pt1 string, pt2 int) PARTITIONED BY (pt1, pt2)")
+      sql(s"INSERT INTO t values(1, 'a', 'a', 1), (2, 'b', 'a', 2), (3, 'c', 'c', 3)")
 
-        var expect = Seq(Row("pt1=a/pt2=1"), Row("pt1=a/pt2=2"), Row("pt1=c/pt2=3"))
-        checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
-        expect = Seq(Row("pt1=a/pt2=1"), Row("pt1=a/pt2=2"))
-        checkAnswer(sql(s"SHOW PARTITIONS t PARTITION (pt1 = 'a')"), expect)
-      }
+      var expect = Seq(Row("pt1=a/pt2=1"), Row("pt1=a/pt2=2"), Row("pt1=c/pt2=3"))
+      checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
+      expect = Seq(Row("pt1=a/pt2=1"), Row("pt1=a/pt2=2"))
+      checkAnswer(sql(s"SHOW PARTITIONS t PARTITION (pt1 = 'a')"), expect)
     }
-
   }
 
   test("add partition") {
-    withCurrentCatalogAndNamespace {
-      withTable("t") {
-        sql("CREATE TABLE t (id int, name string, pt1 string, pt2 int) PARTITIONED BY (pt1, pt2)")
+    withTable("t") {
+      sql("CREATE TABLE t (id int, name string, pt1 string, pt2 int) PARTITIONED BY (pt1, pt2)")
 
-        // add from sparksql
-        sql(s"ALTER TABLE t ADD PARTITION (pt1 = 'b', pt2 = 1)")
-        var expect = Seq(Row("pt1=b/pt2=1"))
-        checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
-        sql(s"ALTER TABLE t ADD IF NOT EXISTS PARTITION (pt1 = 'b', pt2 = 1)")
-        checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
+      // add from sparksql
+      sql(s"ALTER TABLE t ADD PARTITION (pt1 = 'b', pt2 = 1)")
+      var expect = Seq(Row("pt1=b/pt2=1"))
+      checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
+      sql(s"ALTER TABLE t ADD IF NOT EXISTS PARTITION (pt1 = 'b', pt2 = 1)")
+      checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
 
-        // add from fluss
-        val map = Map("pt1" -> "b", "pt2" -> "2")
-        admin.createPartition(createTablePath("t"), new PartitionSpec(map.asJava), false).get()
-        expect = Seq(Row("pt1=b/pt2=1"), Row("pt1=b/pt2=2"))
-        checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
-      }
+      // add from fluss
+      val map = Map("pt1" -> "b", "pt2" -> "2")
+      admin.createPartition(createTablePath("t"), new PartitionSpec(map.asJava), false).get()
+      expect = Seq(Row("pt1=b/pt2=1"), Row("pt1=b/pt2=2"))
+      checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
     }
   }
 
   test("drop partition") {
-    withCurrentCatalogAndNamespace {
-      withTable("t") {
-        sql("CREATE TABLE t (id int, name string, pt1 string, pt2 int) PARTITIONED BY (pt1, pt2)")
-        sql(s"INSERT INTO t values(1, 'a', 'a', 1), (2, 'b', 'a', 2), (3, 'c', 'c', 3)")
+    withTable("t") {
+      sql("CREATE TABLE t (id int, name string, pt1 string, pt2 int) PARTITIONED BY (pt1, pt2)")
+      sql(s"INSERT INTO t values(1, 'a', 'a', 1), (2, 'b', 'a', 2), (3, 'c', 'c', 3)")
 
-        // drop from sparksql
-        sql(s"ALTER TABLE t DROP PARTITION (pt1 = 'a', pt2 = 2)")
-        var expect = Seq(Row("pt1=a/pt2=1"), Row("pt1=c/pt2=3"))
-        checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
-        sql(s"ALTER TABLE t DROP IF EXISTS PARTITION (pt1 = 'a', pt2 = 2)")
-        checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
+      // drop from sparksql
+      sql(s"ALTER TABLE t DROP PARTITION (pt1 = 'a', pt2 = 2)")
+      var expect = Seq(Row("pt1=a/pt2=1"), Row("pt1=c/pt2=3"))
+      checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
+      sql(s"ALTER TABLE t DROP IF EXISTS PARTITION (pt1 = 'a', pt2 = 2)")
+      checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
 
-        // drop from fluss
-        val map = Map("pt1" -> "c", "pt2" -> "3")
-        admin.dropPartition(createTablePath("t"), new PartitionSpec(map.asJava), false).get()
-        expect = Seq(Row("pt1=a/pt2=1"))
-        checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
-      }
+      // drop from fluss
+      val map = Map("pt1" -> "c", "pt2" -> "3")
+      admin.dropPartition(createTablePath("t"), new PartitionSpec(map.asJava), false).get()
+      expect = Seq(Row("pt1=a/pt2=1"))
+      checkAnswer(sql(s"SHOW PARTITIONS t"), expect)
     }
   }
 }
