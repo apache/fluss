d88232e3775c528af1795b8083993277d03dba4a
Rion Williams <rionmonster@gmail.com>
2026-01-20 23:39:44 -0600

    [lake/iceberg] Updated previous rewrite-conflict integration test to align with updated commit behavior

--- fluss-lake/fluss-lake-iceberg/src/test/java/org/apache/fluss/lake/iceberg/testutils/FlinkIcebergTieringTestBase.java
+++ fluss-lake/fluss-lake-iceberg/src/test/java/org/apache/fluss/lake/iceberg/testutils/FlinkIcebergTieringTestBase.java
@@ -358,17 +358,19 @@ public class FlinkIcebergTieringTestBase {
             throws IOException {
         org.apache.iceberg.Table table = icebergCatalog.loadTable(toIceberg(tablePath));
         int count = 0;
+        boolean deleteFileExists = false;
         try (CloseableIterable<FileScanTask> tasks = table.newScan().planFiles()) {
             for (FileScanTask ignored : tasks) {
-                if (shouldDeleteFileExist) {
-                    assertThat(ignored.deletes()).isNotEmpty();
-                } else {
-                    assertThat(ignored.deletes()).isEmpty();
+                // A deletion status may be mixed (some may contain deletions and
+                // others may not) so we want to flag if deletions are present
+                if (shouldDeleteFileExist && !deleteFileExists) {
+                    deleteFileExists = !ignored.deletes().isEmpty();
                 }
                 count++;
             }
         }
         assertThat(count).isEqualTo(expectedFileCount);
+        assertThat(deleteFileExists).isEqualTo(shouldDeleteFileExist);
     }
 
     protected void checkDataInIcebergAppendOnlyPartitionedTable(
