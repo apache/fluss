2610b9e12428d10dfe789927dc8bd75484fe2b1a
Liebing <liebingyu@gmail.com>
2026-03-11 15:45:44 +0800

    [server] Update Tablet Server to use per-table/partition remote data directory

--- fluss-server/src/test/java/org/apache/fluss/server/replica/ReplicaTestBase.java
+++ fluss-server/src/test/java/org/apache/fluss/server/replica/ReplicaTestBase.java
@@ -57,6 +57,7 @@ import org.apache.fluss.server.zk.NOPErrorHandler;
 import org.apache.fluss.server.zk.ZooKeeperClient;
 import org.apache.fluss.server.zk.ZooKeeperExtension;
 import org.apache.fluss.server.zk.data.LeaderAndIsr;
+import org.apache.fluss.server.zk.data.PartitionAssignment;
 import org.apache.fluss.server.zk.data.TableRegistration;
 import org.apache.fluss.testutils.common.AllCallbackWrapper;
 import org.apache.fluss.testutils.common.ManuallyTriggeredScheduledExecutorService;
@@ -131,6 +132,7 @@ public class ReplicaTestBase {
     protected static final int TABLET_SERVER_ID = 1;
     private static final String TABLET_SERVER_RACK = "rack1";
     protected static ZooKeeperClient zkClient;
+    protected static String remoteDataDir;
 
     // to register all should be closed after each test
     private final CloseableRegistry closeableRegistry = new CloseableRegistry();
@@ -165,6 +167,7 @@ public class ReplicaTestBase {
                 ZOO_KEEPER_EXTENSION_WRAPPER
                         .getCustomExtension()
                         .getZooKeeperClient(NOPErrorHandler.INSTANCE);
+        remoteDataDir = zkClient.getDefaultRemoteDataDir();
     }
 
     @BeforeEach
@@ -261,25 +264,31 @@ public class ReplicaTestBase {
         zkClient.registerTable(
                 DATA1_TABLE_PATH,
                 TableRegistration.newTable(
-                        DATA1_TABLE_ID, DEFAULT_REMOTE_DATA_DIR, data1NonPkTableDescriptor));
+                        DATA1_TABLE_ID,
+                        conf.get(ConfigOptions.REMOTE_DATA_DIR),
+                        data1NonPkTableDescriptor));
         zkClient.registerFirstSchema(DATA1_TABLE_PATH, DATA1_SCHEMA);
         zkClient.registerTable(
                 DATA1_TABLE_PATH_PK,
                 TableRegistration.newTable(
-                        DATA1_TABLE_ID_PK, DEFAULT_REMOTE_DATA_DIR, DATA1_TABLE_DESCRIPTOR_PK));
+                        DATA1_TABLE_ID_PK,
+                        conf.get(ConfigOptions.REMOTE_DATA_DIR),
+                        DATA1_TABLE_DESCRIPTOR_PK));
         zkClient.registerFirstSchema(DATA1_TABLE_PATH_PK, DATA1_SCHEMA_PK);
 
         zkClient.registerTable(
                 DATA2_TABLE_PATH,
                 TableRegistration.newTable(
-                        DATA2_TABLE_ID, DEFAULT_REMOTE_DATA_DIR, DATA2_TABLE_DESCRIPTOR));
+                        DATA2_TABLE_ID,
+                        conf.get(ConfigOptions.REMOTE_DATA_DIR),
+                        DATA2_TABLE_DESCRIPTOR));
         zkClient.registerFirstSchema(DATA2_TABLE_PATH, DATA2_SCHEMA);
 
         zkClient.registerTable(
                 DATA3_TABLE_PATH_PK_AUTO_INC,
                 TableRegistration.newTable(
                         DATA3_TABLE_ID_PK_AUTO_INC,
-                        DEFAULT_REMOTE_DATA_DIR,
+                        conf.get(ConfigOptions.REMOTE_DATA_DIR),
                         DATA3_TABLE_DESCRIPTOR_PK_AUTO_INC));
         zkClient.registerFirstSchema(DATA3_TABLE_PATH_PK_AUTO_INC, DATA3_SCHEMA_PK_AUTO_INC);
     }
@@ -499,6 +508,7 @@ public class ReplicaTestBase {
                 NOPErrorHandler.INSTANCE,
                 metricGroup,
                 DATA1_TABLE_INFO,
+                remoteDataDir,
                 manualClock);
     }
 
@@ -572,6 +582,50 @@ public class ReplicaTestBase {
                 .map(f -> f.getPath().getName())
                 .filter(f -> !f.equals("metadata"))
                 .collect(Collectors.toSet());
+    }
+
+    protected TableBucket makeTableBucket(boolean partitionTable) throws Exception {
+        return makeTableBucket(DATA1_TABLE_ID, partitionTable);
+    }
+
+    protected TableBucket makeTableBucket(boolean partitionTable, boolean kvTable)
+            throws Exception {
+        long tableId = kvTable ? DATA1_TABLE_ID_PK : DATA1_TABLE_ID;
+        Long partitionId = partitionTable ? 0L : null;
+        return makeTableBucket(tableId, partitionId, kvTable);
+    }
+
+    protected TableBucket makeTableBucket(long tableId, boolean partitionTable) throws Exception {
+        Long partitionId = partitionTable ? 0L : null;
+        return makeTableBucket(tableId, partitionId, false);
+    }
+
+    protected TableBucket makeTableBucket(long tableId, Long partitionId, boolean kvTable)
+            throws Exception {
+        int bucketId = 0;
+        boolean partitionTable = partitionId != null;
+        if (partitionTable) {
+            if (kvTable) {
+                zkClient.registerPartitionAssignmentAndMetadata(
+                        partitionId,
+                        DATA1_PHYSICAL_TABLE_PATH_PK_PA_2024.getPartitionName(),
+                        new PartitionAssignment(tableId, Collections.emptyMap()),
+                        conf.get(ConfigOptions.REMOTE_DATA_DIR),
+                        DATA1_TABLE_PATH_PK,
+                        tableId);
+            } else {
+                zkClient.registerPartitionAssignmentAndMetadata(
+                        partitionId,
+                        DATA1_PHYSICAL_TABLE_PATH_PA_2024.getPartitionName(),
+                        new PartitionAssignment(tableId, Collections.emptyMap()),
+                        conf.get(ConfigOptions.REMOTE_DATA_DIR),
+                        DATA1_TABLE_PATH,
+                        tableId);
+            }
+            return new TableBucket(tableId, partitionId, bucketId);
+        } else {
+            return new TableBucket(tableId, bucketId);
+        }
     }
 
     /** An implementation of {@link SnapshotContext} for test purpose. */
@@ -659,11 +713,6 @@ public class ReplicaTestBase {
         @Override
         public int getSnapshotFsWriteBufferSize() {
             return 1024;
-        }
-
-        @Override
-        public FsPath getRemoteKvDir() {
-            return remoteKvTabletDir;
         }
 
         @Override
